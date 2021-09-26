fc162df0ec83addda5f4d6bf3eed9433085d7c4a
vitran96 <tranuytrieuvi@gmail.com>
2021-09-26 09:12:24 +0700

    1st commit

---
+++ wslApi-sample/wslApi-sample.cpp
@@ -0,0 +1,26 @@
+//#include "WslApiLoader.h"
+
+#include <WinSock2.h>
+#include <Windows.h>
+#include <winapifamily.h>
+#include <wslapi.h>
+
+#include <iostream>
+
+int main()
+{
+	try
+	{
+		std::wstring distributionName = L"Ubuntu-18.04";
+		BOOL test = WslIsDistributionRegistered(distributionName.c_str());
+		//WslApiLoader apiLoader(distributionName);
+		//auto isResigtered = apiLoader.WslIsDistributionRegistered();
+		//std::wcout << distributionName << " installed? - " << isResigtered << std::endl;
+		return 0;
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+		return 1;
+	}
+}
